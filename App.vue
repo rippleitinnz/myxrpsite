9e8ca955da3d2f6560a4b68b60d8e4c073a45888
rippleitinnz <42262747+rippleitinnz@users.noreply.github.com>
2020-05-02 02:19:28 +1200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <RipHead />
+    <RipBody />
+    <RipFooter />
+  </div>
+</template>
+
+<script>
+import RipHead from "./components/RipHead";
+import RipBody from "./components/RipBody";
+import RipFooter from "./components/RipFooter";
+
+export default {
+  name: "App",
+  components: {
+    RipHead,
+    RipBody,
+    RipFooter
+  }
+};
+</script>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+      overflow-x: hidden;
+    margin-right: calc(-1 * (100vw - 100%));
+}
+
+</style>
